f93b5786c4703ee5071e74c982dec72849c98dbf
nikenhpsr <niken18003@mail.unpad.ac.id>
2024-08-02 21:00:37 +0700

    fix: allowed hosts

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -7,7 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security settings
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-secret-key')
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
